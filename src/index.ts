b076a68a0775e3a2041029b6e18a6e3ec213b387
anugrahp13 <anugrahprastyo13@gmail.com>
2025-04-29 22:00:15 +0700

    feat[index.ts]: create file index for serves as the main entry point of the application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-import express
+import express from "express";
+import accountRoutes from "./routes/account.routes";
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", accountRoutes);
+
+app.get("/", (req,res) => {
+    res.send("Hello Word form E-wallet API");
+});
+
+const PORT =process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`ini server running di port ${PORT}`)
+})
